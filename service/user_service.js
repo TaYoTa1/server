ba8236299a3ee380b744fe46751113e38ae90d1f
TaYoTa1 <gr6371695@gmail.com>
2023-05-18 13:44:44 +0300

    новый файл:    package.json 	новый файл:    service/user_service.js 	.gitignore 	controllers/ 	indexs.js 	package-lock.json 	prisma/ 	router/

---
+++ service/user_service.js
@@ -0,0 +1,53 @@
+const { Prisma, PrismaClient } = require("@prisma/client")
+
+const prisma_client = new PrismaClient
+class UserService{
+    async GetAllUsers()
+    {
+       return prisma_client.user.findMany({});
+    }    
+    
+}
+async function AddUser(data) {
+    const {Id, email, password } = data
+    try {
+      const user = await prisma_client.user.create({
+        where: { id: Id },
+        update: {
+            id,   
+            firstName,
+            lastName,
+            email,
+            numberPhone,
+            position,
+            jobPlace,
+            aboutPC
+        },
+        create: {
+            id,   
+            firstName,
+            lastName,
+            email,
+            numberPhone,
+            position,
+            jobPlace,
+            aboutPC
+        },
+        select: { id: true, email: true }
+      })
+      return user
+    } catch(e) {
+      onError(e)
+    }
+  }
+  /*
+  id,   
+  firstName,
+  lastName,
+  email,
+  numberPhone,
+  position,
+  jobPlace,
+  aboutPC
+  */
+module.exports = new UserService;
